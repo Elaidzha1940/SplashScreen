2ee4e586d308368d1b15db8b9e8de419761c2d7b
Elaidzha1940 <eli080293@gmail.com>
2023-09-07 15:21:38 +0300

    SplashScreen

---
+++ SplashScreen/ContentView.swift
@@ -0,0 +1,28 @@
+//  /*
+//
+//  Project: SplashScreen
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 07.09.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+         
+        }
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SplashScreen/SplashScreenApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: SplashScreen
+//  File: SplashScreenApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 07.09.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+@main
+struct SplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
